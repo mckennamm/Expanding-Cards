d40049fb6bdc79cecacc41c415efe061e5d2d2b3
mckennamm <molly.mckenna88@gmail.com>
2025-03-12 10:50:35 -0400

    completed javascript functionality

--- script.js
+++ script.js
@@ -5,16 +5,15 @@ const panels = document.querySelectorAll('.panel');
 panels.forEach((panel) => {
     panel.addEventListener('click', () => {
         console.log('clicked');
-        panel.classList.add('active');
-    
+        
         removeActiveClasses();
 
-        
+        panel.classList.add('active');
     })
 })
 
 function removeActiveClasses() {
-    panel.forEach((panel) => {
-        panels.classList.remove('active');
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
     })   
 }
